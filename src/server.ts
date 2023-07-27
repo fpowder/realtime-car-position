6088dba5d329fb8563a8156943325216c54a14a0
PADUCK <re____@naver.com>
2023-07-27 17:16:15 +0900

    port

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,36 @@
-import { Server } from 'socket.io';
-import http from 'http';
-import express, { Express } from 'express';
+import { Server } from "socket.io";
+import http from "http";
+import express, { Express } from "express";
 
-import path from 'path';
+import path from "path";
 export const appRoot = path.resolve();
 
-import { sample } from './router/sample';
-import { page } from './router/page';
+import { sample } from "./router/sample";
+import { page } from "./router/page";
 
 // import MqttClient from './mqtt/client';
 // import MqttLog from './mqtt/log';
 
 const app: Express = express();
 const server: http.Server = http.createServer(app);
-const port: number = 3001;
+const port: number = 4004;
 
-app.use('/', page);
-app.use('/sample', sample);
+app.use("/", page);
+app.use("/sample", sample);
 
 export const io = new Server(server, {
-	transports: ['websocket'],
-	cors: {
-		origin: `*`,
-		methods: ['GET', 'POST', 'UPDATE', 'DELETE'],
-	},
+    transports: ["websocket"],
+    cors: {
+        origin: `*`,
+        methods: ["GET", "POST", "UPDATE", "DELETE"],
+    },
 });
 
-io.on('connection', (socket) => {
-	console.log('client connected');
+io.on("connection", (socket) => {
+    console.log("client connected");
 });
 
 server.listen(port, () => {
-	console.log(`server is running on port ${port}`);
+    console.log(`server is running on port ${port}`);
 });
+
